c0f470d3f9156ac4ed6ad3d13cd9292a62f11887
naglicmoc <dvd.filmas@gmail.com>
2024-10-14 10:37:52 +0300

    lists

---
+++ main.py
@@ -0,0 +1,44 @@
+
+#          0  1  2
+numbers = [1, 5, 10]
+
+print(numbers)
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+
+numbers[0] = 6
+
+print(numbers[0])
+
+print(len(numbers))
+#01234
+"labas"
+
+# numbers[3] = 7 #veiktu PHP
+numbers.append(7)
+print(numbers)
+numbers.extend([8, 10])
+print(numbers)
+numbers.insert(1,10)
+print(numbers)
+numbers.pop()
+print(numbers)
+num = numbers.pop(4)
+print(numbers)
+print(num)
+print(numbers.index(10))
+print(5 in numbers)
+print(numbers.count(10))
+print("---")
+sortedNumbers = sorted(numbers)
+print(sortedNumbers)
+print(numbers)
+
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+
+print(numbers[::2])
+print(numbers[1:3])
